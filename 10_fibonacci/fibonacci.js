a9b137339758de69098358cf066edace25df2b29
costa911 <costa.andrea911@gmail.com>
2024-02-26 12:01:06 +0100

    ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    let prev = 0, curr = 1;
+    for (let i = 2; i <= n; i++) [prev, curr] = [curr, prev + curr];
+    return curr;
 };
 
 // Do not edit below this line
